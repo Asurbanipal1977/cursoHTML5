4704a72830061fdb3ee254c6d585c3b5ebb6ca28
Asurbanipal1977 <Asurbanipal1977@gmail.com>
2021-10-08 18:35:50 +0200

    nuevos cambios

--- js/funcionalidadVideo.js
+++ js/funcionalidadVideo.js
@@ -1,30 +1,57 @@
-var mivideo, reproducir, barra, progreso;
+var mivideo, miaudio, mimedia, reproducir, barra, progreso;
+const maximo = 600;
 
 function comenzar()
 {
-    mivideo=document.getElementById("mivideo");
+    if (document.getElementById("mivideo")!=null)
+        mimedia=document.getElementById("mivideo");
+    else
+        mimedia=document.getElementById("miaudio");
+
     reproducir=document.getElementById("reproducir");
     barra=document.getElementById("barra");
     progreso=document.getElementById("progreso");
 
     reproducir.addEventListener("click",() => {
-        if (mivideo.paused || mivideo.ended)
+        if (mimedia.paused || mimedia.ended)
         {
-            mivideo.play();
+            mimedia.play();
             reproducir.innerHTML="Pause";
+
+            setInterval(estado,100);
         }
         else
         {
-            mivideo.pause(); 
+            mimedia.pause(); 
             reproducir.innerHTML="Play";
         }       
     }, false);
 
-    mivideo.addEventListener("ended", () => {reproducir.innerHTML="Play"}, false)
+    mimedia.addEventListener("ended", () => {reproducir.innerHTML="Play"}, false);
 
-    progreso.addEventListener("click",() => {
-        
+    barra.addEventListener("click",(posicion) => {
+        //if (!mimedia.paused && !mimedia.ended)
+        //{
+            var ratonX = posicion.pageX-barra.offsetLeft;
+            var nuevoTiempo = (ratonX*mimedia.duration) / maximo;
+            
+            mimedia.currentTime = nuevoTiempo;
+            progreso.style.width = ratonX + "px";
+        //}
+
+
     }, false);
 }
+
+function estado()
+{
+    if (!mimedia.ended)
+    {
+        var total = parseInt((mimedia.currentTime*maximo)/mimedia.duration,10);
+
+        progreso.style.width=total+"px";
+    }
+}
+
 
 window.addEventListener("load",comenzar,false);
